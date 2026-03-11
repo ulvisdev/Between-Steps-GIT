aeb7905bfe9fc9d1ccf3dc785ef12569239a2a52
ulvisdev <ulvis.svelpe12@gmail.com>
2026-03-11 16:21:38 +0000

    big updates
    Ledge check WORKS, Fixed player dash jump issue (jumps automatically after dashing)

---
+++ Assets/Scripts/LedgeCheck.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class LedgeCheck : MonoBehaviour
+{
+    public Transform leftcheck;
+    public Transform rightcheck;
+    public LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Update()
+    {
+        float ledgePushAmount = 0.005f;
+
+        // // LEFT OUTER
+        // Vector2 lroLEFT = left_outer != null ? (Vector2)left_outer.position : (Vector2)transform.position + groundCheckOffset;
+        // RaycastHit2D hit_leftO = Physics2D.Raycast(lroLEFT, Vector2.up, groundCheckDistance, groundLayer);
+        // bool hitLouter = hit_leftO.collider != null;
+
+        // // LEFT INNER
+        // Vector2 lriLEFT = left_inner != null ? (Vector2)left_inner.position : (Vector2)transform.position + groundCheckOffset;
+        // RaycastHit2D hit_leftI = Physics2D.Raycast(lriLEFT, Vector2.up, groundCheckDistance, groundLayer);
+        // bool hitLinner = hit_leftI.collider != null;
+
+        // // RIGHT OUTER
+        // Vector2 lroRIGHT = right_outer != null ? (Vector2)right_outer.position : (Vector2)transform.position + groundCheckOffset;
+        // RaycastHit2D hit_rightO = Physics2D.Raycast(lroRIGHT, Vector2.up, groundCheckDistance, groundLayer);
+        // bool hitRouter = hit_rightO.collider != null;
+
+        // // RIGHT INNER
+        // Vector2 lriRIGHT = right_inner != null ? (Vector2)right_inner.position : (Vector2)transform.position + groundCheckOffset;
+        // RaycastHit2D hit_rightI = Physics2D.Raycast(lriRIGHT, Vector2.up, groundCheckDistance, groundLayer);
+        // bool hitRinner = hit_rightI.collider != null;
+
+        bool hitLeft = Physics2D.OverlapBox(leftcheck.position, new Vector2(0.25f, 0.25f), 0f, groundLayer);
+        bool hitRight = Physics2D.OverlapBox(rightcheck.position, new Vector2(0.25f, 0.25f), 0f, groundLayer);
+
+        // Push away from the ledge
+        if (rb.linearVelocity.y > 0f)
+        {
+            if (hitLeft)
+                rb.position += Vector2.right * ledgePushAmount;
+
+            if (hitRight)
+                rb.position += Vector2.left * ledgePushAmount;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 Can check jump animation also with y velocity, but need 2 animations - jump up and fall down.
 
-
+MAYBE Need to add more Colliders if / else if / else if (closer to player center overrides to snap bigger distance)
 */
 
 public class PlayerController : MonoBehaviour
@@ -29,8 +29,8 @@ public class PlayerController : MonoBehaviour
 
     public Transform groundCheckLEFT;
     public Transform groundCheckRIGHT;
-
     public LayerMask groundLayer;
+
     private Rigidbody2D rb;
     private float XInput;
     private float YInput;
@@ -62,21 +62,21 @@ public class PlayerController : MonoBehaviour
     private bool isJumpFalling = false;
 
     //wall jumping
-/*    private bool isWallJumping;
-    private float wallJumpingDir;
-    [SerializeField] private float wallJumpingTime = 0.2f;
-    private float wallJumpingCounter;
-    [SerializeField] private float wallJumpingDuration = 0.4f;
-    [SerializeField] private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+    /*    private bool isWallJumping;
+        private float wallJumpingDir;
+        [SerializeField] private float wallJumpingTime = 0.2f;
+        private float wallJumpingCounter;
+        [SerializeField] private float wallJumpingDuration = 0.4f;
+        [SerializeField] private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
-    //wall sliding
-    private bool isWallSliding;
-    [SerializeField] private float wallSlidingSpeed = 8f;
-    [SerializeField] private Transform RightWallCheck;
-    [SerializeField] private LayerMask wallLayer; */
+        //wall sliding
+        private bool isWallSliding;
+        [SerializeField] private float wallSlidingSpeed = 8f;
+        [SerializeField] private Transform RightWallCheck;
+        [SerializeField] private LayerMask wallLayer; */
 
     public TilemapSwitch tilemapswitch;
-    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -97,7 +97,7 @@ public class PlayerController : MonoBehaviour
         YInput = Input.GetAxisRaw("Vertical");
         var dashInput = Input.GetButtonDown("Dash") /*|| Input.GetKeyDown(KeyCode.Keypad4)*/;
 
-// DASH -----------------------------------------------------------------
+        // DASH -----------------------------------------------------------------
 
         if (dashInput && canDash)
         {
@@ -131,9 +131,9 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-// MOVEMENT -----------------------------------------------------------------
+        // MOVEMENT -----------------------------------------------------------------
 
-    // CHECK IF GROUNDED ----------------------------------------------------
+        // CHECK IF GROUNDED ----------------------------------------------------
 
         //MIDDLE
         Vector2 rayOrigin = groundCheck != null ? (Vector2)groundCheck.position : (Vector2)transform.position + groundCheckOffset;
@@ -141,21 +141,21 @@ public class PlayerController : MonoBehaviour
         //isGrounded = hit.collider != null && !(rb.linearVelocity.y > 0.01f);
 
         //LEFT
-        Vector2 rayOriginLEFT = groundCheck != null ? (Vector2)groundCheckLEFT.position : (Vector2)transform.position + groundCheckOffset;
+        Vector2 rayOriginLEFT = groundCheckLEFT != null ? (Vector2)groundCheckLEFT.position : (Vector2)transform.position + groundCheckOffset;
         RaycastHit2D hitLEFT = Physics2D.Raycast(rayOriginLEFT, Vector2.down, groundCheckDistance, groundLayer);
 
         //RIGHT
-        Vector2 rayOriginRIGHT = groundCheck != null ? (Vector2)groundCheckRIGHT.position : (Vector2)transform.position + groundCheckOffset;
+        Vector2 rayOriginRIGHT = groundCheckRIGHT != null ? (Vector2)groundCheckRIGHT.position : (Vector2)transform.position + groundCheckOffset;
         RaycastHit2D hitRIGHT = Physics2D.Raycast(rayOriginRIGHT, Vector2.down, groundCheckDistance, groundLayer);
 
         //bool hitEDGE = hitLEFT.collider || hitRIGHT.collider;
         //isGroundedEDGE = hitEDGE && !(rb.linearVelocity.y > 0.01f);
 
-        isGrounded = (hit.collider != null 
-                        || hitLEFT.collider != null 
+        isGrounded = (hit.collider != null
+                        || hitLEFT.collider != null
                         || hitRIGHT.collider != null) && !(rb.linearVelocity.y > 0.01f);
 
-    // ACCELERATION ---------------------------------------------------------
+        // ACCELERATION ---------------------------------------------------------
 
         targetSpeed = XInput * maxRunSpeed;
 
@@ -164,7 +164,7 @@ public class PlayerController : MonoBehaviour
         else
             accelRate = runDeceleration;
 
-    // COYOTE ---------------------------------------------------------------
+        // COYOTE ---------------------------------------------------------------
 
         if (isGrounded /*&& isGroundedEDGE*/)
         {
@@ -173,13 +173,13 @@ public class PlayerController : MonoBehaviour
         }
         else coyoteTimeCounter -= Time.deltaTime;
 
-    // JUMP BUFFER ----------------------------------------------------------
+        // JUMP BUFFER ----------------------------------------------------------
 
-        if (Input.GetButtonDown("Jump") /*|| Input.GetKeyDown(KeyCode.W)*/) 
+        if (Input.GetButtonDown("Jump") /*|| Input.GetKeyDown(KeyCode.W)*/)
         {
             jumpBufferCounter = jumpBufferTime;
         }
-        else 
+        else
         {
             jumpBufferCounter -= Time.deltaTime;
         }
@@ -192,11 +192,11 @@ public class PlayerController : MonoBehaviour
             {
                 AudioManager.Instance.PlaySFX(AudioManager.Instance.jumpSFX);
             }
-            
+
             jumpBufferCounter = 0f;
         }
 
-    // HIGHER JUMP ON HOLD -------------------------------------------------
+        // HIGHER JUMP ON HOLD -------------------------------------------------
 
         if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
         {
@@ -205,29 +205,29 @@ public class PlayerController : MonoBehaviour
             coyoteTimeCounter = 0f;
         }
 
-// SPRITE FLIP --------------------------------------------------------------
+        // SPRITE FLIP --------------------------------------------------------------
 
         //if (!isWallJumping)
         if (spriteRenderer != null)
         {
-            if(XInput > .1f) spriteRenderer.flipX = false;
-            if(XInput < -.1f) spriteRenderer.flipX = true;
+            if (XInput > .1f) spriteRenderer.flipX = false;
+            if (XInput < -.1f) spriteRenderer.flipX = true;
         }
 
-// ANIMATION VARIABLES ------------------------------------------------------
+        // ANIMATION VARIABLES ------------------------------------------------------
 
         if (anim != null)
         {
-        anim.SetFloat("Speed", Mathf.Abs(XInput));
-        anim.SetFloat("YSpeed", rb.linearVelocity.y);
-        anim.SetBool("isGrounded", isGrounded);
-        //anim.SetBool("isGroundedEDGE", isGroundedEDGE);
+            anim.SetFloat("Speed", Mathf.Abs(XInput));
+            anim.SetFloat("YSpeed", rb.linearVelocity.y);
+            anim.SetBool("isGrounded", isGrounded);
+            //anim.SetBool("isGroundedEDGE", isGroundedEDGE);
         }
 
-    // FAST FALL ON JUMP -----------------------------------------------------
+        // FAST FALL ON JUMP -----------------------------------------------------
 
-    float desiredGravity = defaultGravityScale;
-    bool falling = rb.linearVelocity.y < -0.01f;
+        float desiredGravity = defaultGravityScale;
+        bool falling = rb.linearVelocity.y < -0.01f;
 
         if (falling)
         {
@@ -238,11 +238,11 @@ public class PlayerController : MonoBehaviour
         else
             isJumpFalling = false;
 
-    bool inAir = !isGrounded /*&& !isGroundedEDGE*/;
-    bool nearY0 = Mathf.Abs(rb.linearVelocity.y) < jumpHangTimeThreshold;
-    bool inJumping = inAir && nearY0;
+        bool inAir = !isGrounded /*&& !isGroundedEDGE*/;
+        bool nearY0 = Mathf.Abs(rb.linearVelocity.y) < jumpHangTimeThreshold;
+        bool inJumping = inAir && nearY0;
 
-    // HOLD MID-AIR ----------------------------------------------------------
+        // HOLD MID-AIR ----------------------------------------------------------
 
         if (inJumping)
         {
@@ -251,13 +251,10 @@ public class PlayerController : MonoBehaviour
             targetSpeed *= jumpHangMaxSpeedMult;
         }
 
-    rb.gravityScale = desiredGravity;
+        rb.gravityScale = desiredGravity;
 
-    //WallSlide();
-    //WallJump();
-
-
-
+        //WallSlide();
+        //WallJump();
     }
 
     public void PlayRunSFX()
@@ -277,23 +274,23 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = new Vector2(XInput * speed, rb.linearVelocity.y);
     }*/
 
-void FixedUpdate()
-{
-    if (isDashing) return;
+    void FixedUpdate()
+    {
+        if (isDashing) return;
 
-    float maxSpeedChange = accelRate * Time.fixedDeltaTime;
-    float newX = Mathf.MoveTowards(rb.linearVelocity.x, targetSpeed, maxSpeedChange);
+        float maxSpeedChange = accelRate * Time.fixedDeltaTime;
+        float newX = Mathf.MoveTowards(rb.linearVelocity.x, targetSpeed, maxSpeedChange);
 
-    //if (!isWallJumping)
+        //if (!isWallJumping)
         rb.linearVelocity = new Vector2(newX, rb.linearVelocity.y);
 
-    // CLAMP FALL SPEED ---------------------------------------------------
+        // CLAMP FALL SPEED ---------------------------------------------------
 
-    if (rb.linearVelocity.y < -maxFallSpeed)
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, -maxFallSpeed);
-}
+        if (rb.linearVelocity.y < -maxFallSpeed)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -maxFallSpeed);
+    }
 
-// WALL SLIDE -------------------------------------------------------------
+    // WALL SLIDE -------------------------------------------------------------
     /*
     private bool isWalled()
     {
@@ -352,7 +349,7 @@ void FixedUpdate()
     }
     */
 
-// DASH ----------------------------------------------------------------
+    // DASH ----------------------------------------------------------------
     private IEnumerator StopDashing()
     {
         yield return new WaitForSeconds(dashingTime);
@@ -360,13 +357,16 @@ void FixedUpdate()
         isDashing = false;
 
         //stops dash momentum
+        if (rb.linearVelocity.y > 0f)
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, defaultGravityScale * 1.5f);
+
     }
 
 
-// GIZMOS --------------------------------------------------------------
+    // GIZMOS --------------------------------------------------------------
     void OnDrawGizmosSelected()
     {
+        // MIDDLE GROUND CHECK
         if (groundCheck != null)
         {
             Gizmos.color = Color.yellow;
@@ -378,6 +378,7 @@ void FixedUpdate()
             Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.down * groundCheckDistance);
         }
 
+        // LEFT GROUND CHECK
         if (groundCheckLEFT != null)
         {
             Gizmos.color = Color.yellow;
@@ -389,6 +390,7 @@ void FixedUpdate()
             Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.down * groundCheckDistance);
         }
 
+        // RIGHT GROUND CHECK
         if (groundCheckRIGHT != null)
         {
             Gizmos.color = Color.yellow;
@@ -399,5 +401,53 @@ void FixedUpdate()
             Gizmos.color = Color.yellow;
             Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.down * groundCheckDistance);
         }
+
+        // LEFT OUTER LEDGE CHECK
+        // if (left_outer != null)
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(left_outer.position, left_outer.position + Vector3.up * groundCheckDistance);
+        // }
+        // else
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.up * groundCheckDistance);
+        // }
+
+        // // LEFT INNER LEDGE CHECK
+        // if (left_inner != null)
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(left_inner.position, left_inner.position + Vector3.up * groundCheckDistance);
+        // }
+        // else
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.up * groundCheckDistance);
+        // }
+
+        // // RIGHT OUTER LEDGE CHECK
+        // if (right_outer != null)
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(right_outer.position, right_outer.position + Vector3.up * groundCheckDistance);
+        // }
+        // else
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.up * groundCheckDistance);
+        // }
+
+        // // RIGHT INNER LEDGE CHECK
+        // if (right_inner != null)
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(right_inner.position, right_inner.position + Vector3.up * groundCheckDistance);
+        // }
+        // else
+        // {
+        //     Gizmos.color = Color.yellow;
+        //     Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.up * groundCheckDistance);
+        // }
     }
 }
